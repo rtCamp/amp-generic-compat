91b607c9031844a4ec4cb7ecc0ce2fd18bf6c41d
Milind More <milindmore22@gmail.com>
2021-01-12 00:14:30 +0530

    Added backend fields for amp-script and CSS.

--- admin/amp-generic-settings.php
+++ admin/amp-generic-settings.php
@@ -23,19 +23,167 @@ if ( is_admin() ) {
  * @return null | void
  */
 function register_amp_compat_admin_scripts( $hook ) {
-	if ( 'amp_page_amp-compatibility-settings' !== $hook ) {
+
+	if ( ! in_array( $hook, array( 'toplevel_page_amp-compatibility-settings', 'amp-compatibility_page_amp-compatibility-settings-js', 'amp-compatibility_page_amp-compatibility-settings-css' ), true ) ) {
 		return;
 	}
+
 	wp_enqueue_script( 'jquery' );
+
+	$amp_settings = false;
+
+	if ( 'amp-compatibility_page_amp-compatibility-settings-js' === $hook ) {
+		$amp_settings = wp_enqueue_code_editor( array( 'type' => 'text/js' ) );
+		wp_add_inline_script(
+			'code-editor',
+			sprintf(
+				'jQuery( function() { wp.codeEditor.initialize( "amp-compat-js", %s ); } );',
+				wp_json_encode( $amp_settings )
+			)
+		);
+	}
+
+	if ( 'amp-compatibility_page_amp-compatibility-settings-css' === $hook ) {
+		$amp_settings = wp_enqueue_code_editor( array( 'type' => 'text/css' ) );
+		wp_add_inline_script(
+			'code-editor',
+			sprintf(
+				'jQuery( function() { wp.codeEditor.initialize( "amp-compat-css", %s ); } );',
+				wp_json_encode( $amp_settings )
+			)
+		);
+	}
+
+	// Bail if user disabled CodeMirror.
+	if ( false === $amp_settings ) {
+		return;
+	}
+
 	wp_enqueue_script( 'amp-generic-settings', plugin_dir_url( __FILE__ ) . 'js/amp-admin.js', array( 'jquery' ), rand(), true );
 	wp_enqueue_style( 'amp-generic-settings', plugin_dir_url( __FILE__ ) . 'css/amp-admin.css', '', rand() );
+}
+
+/**
+ * Add AMP javascript.
+ */
+function amp_compatibility_page_js() {
+	$amp_compat_enable_js = get_option( 'amp_compat_js' );
+	?>
+	<div class="wrap">
+		<h2><?php esc_html_e( 'Add Javascript to AMP pages' ); ?></h2>
+		<div class="amp-compat-container">
+			<form method="post">
+				<table class="form-table">
+					<tr>
+						<th>
+							<?php esc_html_e( 'Enable' ); ?>
+						</th>
+						<td>
+							<input type="checkbox" value="1" name="amp_compat_enable_js" <?php checked( '1', $amp_compat_enable_js, true ); ?> />
+						</td>
+					</tr>
+				</table>
+				<table class="">
+					<tr>
+						<th>
+							<?php esc_html_e( 'code box' ); ?>
+						</th>
+						<th>
+							<?php esc_html_e( 'Rules' ); ?>
+						</th>
+					</tr>
+					<tr>
+						<td width="80%">
+							<textarea name="amp_compat_js" id="amp-compat-js"></textarea>
+						</td>
+						<td width="20%">
+							<ol>
+								<li>Element getters like <code>getElementByName()</code>, <code>getElementsByClassName()</code>, <code>getElementsByTagName()</code>, <code>childNodes()</code>, <code>parentNode()</code>, and <code>lastChild()</code></li>
+								<li>Mutators like <code>createTextNode()</code>, <code>appendChild()</code>, <code>insertBefore()</code>, <code>removeChild()</code>, and <code>replaceChild()</code></li>
+								<li>Methods involving events like <code>addEventListener()</code>, <code>removeEventListener()</code>, and <code>createEvent()</code></li>
+								<li>Property and attribute getters like <code>getAttribute()</code>, <code>hasAttribute()</code></li>
+								<li>Event properties like <code>Event.target</code>, <code>Event.type</code>, and <code>Event.bubbles</code></li>
+								<li>Element properties like <code>attributes</code>, <code>id</code>, <code>outerHTML</code>, <code>textContent</code>, <code>value</code>, <code>classList</code>, and <code>className</code></li>
+								<li>And many more.</li>
+							</ol>
+						</td>
+					</tr>
+				</table>
+				<div class="clearfix">
+					<?php wp_nonce_field( 'amp_compat_save_settings_action', 'amp_compat_save_settings_action' ); ?>
+					<button type="submit" class="button button-primary" name="save_amp_compat_settings" value="1"><?php esc_html_e( 'Save' ); ?></button>
+				</div>
+			</form>	
+		</div>
+	</div>
+	<?php
+}
+
+
+/**
+ * Add AMP javascript.
+ */
+function amp_compatibility_page_css() {
+	$amp_compat_enable_js = get_option( 'amp_compat_css' );
+	?>
+	<div class="wrap">
+		<h2><?php esc_html_e( 'Add CSS to AMP pages' ); ?></h2>
+		<div class="amp-compat-container">
+			<form method="post">
+				<table class="form-table">
+					<tr>
+						<th>
+							<?php esc_html_e( 'Enable' ); ?>
+						</th>
+						<td>
+							<input type="checkbox" value="1" name="amp_compat_enable_css" <?php checked( '1', $amp_compat_enable_js, true ); ?> />
+						</td>
+					</tr>
+				</table>
+				<table class="">
+					<tr>
+						<th>
+							<?php esc_html_e( 'code box' ); ?>
+						</th>
+						<th>
+							<?php esc_html_e( 'Rules' ); ?>
+						</th>
+					</tr>
+					<tr>
+						<td width="80%">
+							<textarea name="amp_compat_css" id="amp-compat-css"></textarea>
+						</td>
+						<td width="20%">
+							<ol>
+								<li>Element getters like <code>getElementByName()</code>, <code>getElementsByClassName()</code>, <code>getElementsByTagName()</code>, <code>childNodes()</code>, <code>parentNode()</code>, and <code>lastChild()</code></li>
+								<li>Mutators like <code>createTextNode()</code>, <code>appendChild()</code>, <code>insertBefore()</code>, <code>removeChild()</code>, and <code>replaceChild()</code></li>
+								<li>Methods involving events like <code>addEventListener()</code>, <code>removeEventListener()</code>, and <code>createEvent()</code></li>
+								<li>Property and attribute getters like <code>getAttribute()</code>, <code>hasAttribute()</code></li>
+								<li>Event properties like <code>Event.target</code>, <code>Event.type</code>, and <code>Event.bubbles</code></li>
+								<li>Element properties like <code>attributes</code>, <code>id</code>, <code>outerHTML</code>, <code>textContent</code>, <code>value</code>, <code>classList</code>, and <code>className</code></li>
+								<li>And many more.</li>
+							</ol>
+						</td>
+					</tr>
+				</table>
+				<div class="clearfix">
+					<?php wp_nonce_field( 'amp_compat_save_settings_action', 'amp_compat_save_settings_action' ); ?>
+					<button type="submit" class="button button-primary" name="save_amp_compat_settings" value="1"><?php esc_html_e( 'Save' ); ?></button>
+				</div>
+			</form>	
+		</div>
+	</div>
+	<?php
 }
 
 /**
  * Register Sub menu.
  */
 function register_amp_compat_admin_menus() {
-	add_submenu_page( 'amp-options', 'AMP Compatibility', 'AMP Compatibility', 'manage_options', 'amp-compatibility-settings', __NAMESPACE__ . '\amp_compatibility_page' );
+	add_menu_page( 'AMP Compatibility', 'AMP Compatibility', 'manage_options', 'amp-compatibility-settings', __NAMESPACE__ . '\amp_compatibility_page', 'dashicons-hammer' );
+	add_submenu_page( 'amp-compatibility-settings', 'AMP Toggle', 'AMP Toggle', 'manage_options', 'amp-compatibility-settings', __NAMESPACE__ . '\amp_compatibility_page' );
+	add_submenu_page( 'amp-compatibility-settings', 'Add JS', 'JS', 'manage_options', 'amp-compatibility-settings-js', __NAMESPACE__ . '\amp_compatibility_page_js' );
+	add_submenu_page( 'amp-compatibility-settings', 'Add CSS', 'CSS', 'manage_options', 'amp-compatibility-settings-css', __NAMESPACE__ . '\amp_compatibility_page_css' );
 }
 
 /**
